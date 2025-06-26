092a6c54e806f47e168feeb8b91021e00074395e
tomasgrosso4 <tomasgrosso4@gmail.com>
2025-06-26 23:16:49 +0100

    Delete PAP-GMS/GMS directory

--- PAP-GMS/GMS/gms/js/carrinho.js
+++
@@ -1,8 +0,0 @@
-
-const carrinhoLista = document.getElementById('carrinho-lista');
-const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-carrinho.forEach(item => {
-  const li = document.createElement('li');
-  li.textContent = item;
-  carrinhoLista.appendChild(li);
-});
